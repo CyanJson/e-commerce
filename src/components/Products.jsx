33e051f6e0ba55d7341fcc1a4adfe7f87a2eedd6
CyanJson <jirachi0610@gmail.com>
2024-03-12 00:08:19 +0800

    Shopping Cart Update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import shoppingcart from '../assets/shopping-cart.svg';
+import shoppingcartcheck from '../assets/shopping-cart-check.svg';
 
 const Products = ({productImage, productName, productSize, productPrice, addToCart}) => {
+    const [added, setAdded] = useState(false);
+
     return(
         <div className="product">
             <div 
@@ -12,9 +16,15 @@ const Products = ({productImage, productName, productSize, productPrice, addToCa
                 <span>Size: {productSize}</span>
                 <span>{productPrice} PHP</span>
             </div>
-            <button onClick={addToCart}>
-                <img src={shoppingcart} width="18" height="18" alt="shopping-cart" />
-                add to cart
+            <button 
+                onClick={() => {
+                    if(!added) addToCart(); 
+                    setAdded(true); 
+                }}
+                style={{ opacity: added ? .7 : '', boxShadow: added ? 'none' : ''}}
+            >
+                <img src={added ? shoppingcartcheck : shoppingcart} width="18" height="18" alt="shopping-cart" />
+                {added ? 'added' : 'add to cart'}
             </button>
         </div>
     )
